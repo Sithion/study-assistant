0b5d20e3640cae300a45f24e617e958151b7e3ba
Raphael <rrdtorres@gmail.com>
2015-12-09 22:17:24 -0200

    Criação do serviço de usuário

---
+++ api/services/user.service.js
@@ -0,0 +1,21 @@
+var userService = {
+
+	create: function (user) {
+
+	},
+	update: function (user) {
+
+	},
+	delete: function (user) {
+
+	},
+	get: function (user) {
+
+	},
+	list: function (user) {
+
+	}
+};
+
+
+module.exports = userService;
